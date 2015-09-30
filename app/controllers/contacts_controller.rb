50ed78a39d2c91c4ddfcd9117d82aba239a1adc3
Zach McCall <zmccall58@yahoo.com>
2015-09-30 23:32:34 +0000

    Another error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,8 @@ class ContactsController < ApplicationController
       @contact = Contact.new(contact_params)
       
       if @contact.save
-        name = params [:contact][:name]
-        email = params [:contact][:email]
+        name = params[:contact][:name]
+        email = params[:contact][:email]
         body = params[:contact][:comments]
         
         ContactMailer.contact_email(name, email, body).deliver
